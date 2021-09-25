fdca9c5eb1476e0208cf6de458850fa993a29082
Felipe Bonatti <bonattife@gmail.com>
2021-09-25 14:11:39 -0300

    Novos processos de interacao

---
+++ basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Ola PHP</div>
+
+<h2>Exemplo A </h2>
+<?php 
+echo 'Olá ';
+echo 'Mundo!';
+?> 
+
+<h2>Exemplo B </h2>
+<?= "Outra forma de me 'expressar'!"?>
+
+<?php 
+phpinfo();
